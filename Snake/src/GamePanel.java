901ee82055aed38770246d2a0f8f1c2eb9d92b4b
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2024-11-21 16:08:32 -0500

    Initialized variables for snake and food

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -11,4 +11,17 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
     // ^ Calculates total number of grid squares in the panel, Area (360,000px) / unit_size (625px)
     static final int DELAY = 75; // game speed
 
+    // Snake and food variables
+    final int[] x = new int[GAME_UNITS]; // Snakes x-coordinates
+    final int[] y = new int[GAME_UNITS]; // Snakes y-coordinates
+    int bodyParts = 6; // Initial length of snake
+    int applesEaten; // Score
+    int appleX; // x-coordinate of apple
+    int appleY; // y-coordinate of apple
+    char direction = 'R'; // Initial direction: 'R' (Right)
+    boolean running = false;
+    Timer timer;
+
+    // Inital Constructor
+
 }
