1d9c083190b6ad5f49c9f0c19e846d25a59bb4c9
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2024-11-21 15:53:15 -0500

    Set up main class and JFrame boilerplate

--- Snake/src/SnakeGame.java
+++ Snake/src/SnakeGame.java
@@ -1,2 +1,14 @@
-import java.awt.*;
-import javax.swing.*;e
+import javax.swing.*;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Snake Game");
+        GamePanel gamePanel = new GamePanel(); // Reference to seperate GamePanel class
+
+        frame.add(gamePanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
