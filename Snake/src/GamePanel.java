30c096da12b92d8416cd1a10ba4d268f6fece971
Reggie Brown <66994867+ReggieBot@users.noreply.github.com>
2024-11-21 16:36:41 -0500

    created 'draw

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -45,8 +45,35 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
         appleY = (int) (Math.random() * (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
     }
 
+    // Draw the game objects
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        if (running) {
+            // Draw Grid
+            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
+
+            // Draw Apple
+            g.setColor(Color.RED);
+            g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+            // Draw Snake
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) { // Snakes head
+                    g.setColor(Color.GREEN);
+                } else {
+                    g.setColor(new Color(45, 180, 0));
+                }
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
 
 
-
-
+        }
+    }
 }
